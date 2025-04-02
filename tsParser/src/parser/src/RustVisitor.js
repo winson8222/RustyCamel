6e6a8c9494df26e3d40e94ace6a6165e88f02d7a
shashahchk <shashaminchok@gmail.com>
2025-04-02 11:38:33 +0800

    feat: compile ts to js for rust imple

---
+++ tsParser/src/parser/src/RustVisitor.js
@@ -0,0 +1,12 @@
+// Generated from src/Rust.g4 by ANTLR 4.13.1
+import { AbstractParseTreeVisitor } from "antlr4ng";
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by `RustParser`.
+ *
+ * @param <Result> The return type of the visit operation. Use `void` for
+ * operations with no return type.
+ */
+export class RustVisitor extends AbstractParseTreeVisitor {
+}
+//# sourceMappingURL=RustVisitor.js.map
