e7da26bd5e66fb21a8236adeb4e12121bb3fb9d7
shashahchk <shashaminchok@gmail.com>
2025-04-04 17:34:58 +0800

    fix: use named params in mli

--- vm/lib/heap.mli
+++ vm/lib/heap.mli
@@ -1,12 +1,5 @@
 type t
 
-type config = { 
-  heap_size_words: int;
-  word_size: int;
-  tag_offset: int; 
-  size_offset: int;
-} 
-
 type node_tag = 
   | False_tag 
   | True_tag  
@@ -23,24 +16,31 @@ type node_tag =
   | Builtin_tag        
 [@@deriving enum]
 
+type config = { 
+  heap_size_words: int;
+  word_size: int;
+  tag_offset: int; 
+  size_offset: int;
+} 
+
 val initial_config: config
 val create :  t (* TODO: Take in heap size as arg *)
 
-val heap_env_extend : t -> int -> int (* returns new address of the head env *)
+val heap_env_extend : t -> new_frame_addr:int -> int (* returns new address of the head env *)
 
 val heap_get_word : t -> int -> float
-val heap_set_word : t -> int -> float -> unit
+val heap_set_word : t -> address:int -> word:float -> unit
 
-val heap_get_child : t -> int -> int -> float
-val heap_set_child : t -> int -> int -> float -> unit
+val heap_get_child : t -> address:int -> child_index:int -> float
+val heap_set_child : t -> address:int -> child_index:int -> value:float -> unit
 
-val heap_get_tag : t -> int -> int
+val heap_get_tag : t -> int -> node_tag
 
 val heap_get_size : t -> int -> int
 val heap_set_tag : t -> int -> node_tag -> unit
 
 val heap_set_size : t -> int -> int -> unit
 
-val heap_allocate : t -> int -> node_tag -> unit
+val heap_allocate : t -> size:int -> tag:node_tag -> int
 
 val debug_print_bytes : t -> int -> int -> unit
