14367d5856020d451cc240bd78eca929fc3d0a13
shashahchk <shashaminchok@gmail.com>
2025-04-18 17:37:23 +0800

    fix: change typechecker to pass state up instead of down and ensure throw err when ret is void even though expected ret

--- vm/test/type_checker_test.ml
+++ vm/test/type_checker_test.ml
@@ -143,8 +143,27 @@ let test_fun_fails () =
          })
   in
   let actual = check_type node tc in
-  let expected = Error "Return type mismatch" in
+  let expected = Error "Function should return Types.TUndefined but returns Types.TInt" in
   Alcotest.(check (result unit string)) "test" expected actual
+
+  let test_fun_no_ret_fails () =
+    let tc = create () in
+    let open Vm.Ast in
+    let node =
+      Block
+        (Fun
+           {
+             sym = "f";
+             prms = [ "a" ];
+             declared_type = TFunction { ret = TInt; prms = [ TInt ] };
+             body = (Literal (Int 1));
+           })
+    in
+    let actual = check_type node tc in
+    let expected = Error "Missing return in function body" in
+    Alcotest.(check (result unit string)) "test" expected actual
+
+    
 
 let test_fun_compatible_prms_args_succeeds () =
   let tc = create () in
@@ -226,6 +245,7 @@ let () =
       ( "Type checker",
         [
           test_case "Literal int" `Quick test_literal_mismatch_fails;
+          test_case "test_fun_no_ret_fails" `Quick test_fun_no_ret_fails;
           test_case "Literal borrow" `Quick test_literal_borrow_succeeds;
           test_case "test_literal_borrow_and_deref_succeeds" `Quick
             test_literal_borrow_and_deref_succeeds;
